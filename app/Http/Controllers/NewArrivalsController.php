00936193264a5e0d687a33fdabb5c5f14f94c90d
SunGoat1001 <ngoctien10012004@gmail.com>
2024-12-23 21:55:57 +0700

    #10 Refactor NewArrivals Controller to fetch data

--- app/Http/Controllers/NewArrivalsController.php
+++ app/Http/Controllers/NewArrivalsController.php
@@ -10,18 +10,7 @@ class NewArrivalsController extends Controller
     public function index()
     {
         $categories = Category::all();
-        $products = Product::query()
-            ->when(request('search'), function ($query, $search) {
-                return $query->where('name', 'like', '%'.trim($search).'%');
-            })
-            ->when(request('sort'), function ($query, $sort) {
-                return match (strval($sort)) {
-                    'lowestprice' => $query->orderBy('price', 'asc'),
-                    'highestprice' => $query->orderBy('price', 'desc'),
-                    default => $query,
-                };
-            })
-            ->get();
+        $products = Product::all();
 
         return view('new-arrivals', [
             'categories' => $categories,
