3ebe72c38b74f37034eda3693c570d4230ad3d55
tuyenrickchoi <gaxuongban@gmail.com>
2024-12-26 21:15:17 +0700

    #6 - update all contact page x-layout

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -11,15 +11,18 @@ class ContactController extends Controller
     {
         // Validate dữ liệu từ form
         $validated = $request->validate([
-            'name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',    
             'email' => 'required|email',
             'message' => 'required|string',
         ]);
         // Xử lý dữ liệu (ví dụ: lưu vào database hoặc gửi email)
         // Contact::create($validated);
-            Contact::create($validated);
-
-        // Trả về thông báo thành công
-        return redirect()->back()->with('success', 'Cảm ơn bạn đã liên hệ! Chúng tôi sẽ phản hồi sớm nhất có thể.');
+        Contact::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'message' => $request->message,
+        ]);
     }
 }
