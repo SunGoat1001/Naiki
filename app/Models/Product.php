9bdcb536ee81bff20b305f603fec733e0e120541
SunGoat1001 <ngoctien10012004@gmail.com>
2025-01-04 22:14:45 +0700

    refactor: update User model with mass assignable attributes and methods; clean up Category and Product models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
         'name',
         'price',
@@ -20,8 +20,8 @@ class Product extends Model
     ];
 
     public const DEFAULT_CURRENCY = 'VNĐ';
-    public const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1509042239860-f550ce710b93';
 
+    public const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1509042239860-f550ce710b93';
 
     public function category()
     {
@@ -35,18 +35,18 @@ class Product extends Model
      */
     public function getFormattedPriceAttribute()
     {
-        return number_format($this->price, 0, ',', '.') . ' ' . self::DEFAULT_CURRENCY;
+        return number_format($this->price, 0, ',', '.').' '.self::DEFAULT_CURRENCY;
     }
 
     /**
      * Tính tổng giá trị dựa trên số lượng.
      *
-     * @param int $quantity
+     * @param  int  $quantity
      * @return string
      */
     public function getFormattedTotalAmount($quantity = 1)
     {
-        return number_format($this->price * $quantity, 0, ',', '.') . ' ' . self::DEFAULT_CURRENCY;
+        return number_format($this->price * $quantity, 0, ',', '.').' '.self::DEFAULT_CURRENCY;
     }
 
     /**
