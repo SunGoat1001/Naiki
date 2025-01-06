89e085ef89bdea6e8445d0b7639c0dd00391ef7c
SunGoat1001 <ngoctien10012004@gmail.com>
2025-01-06 14:17:12 +0700

    add cache logic for Homepage

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Cache;
 
 class HomepageController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
+        $products = Cache::remember('homepage_data', 3600, function () {
+            return Product::select('id', 'name', 'price', 'main_image_url')
+                ->orderBy('imported_date', 'desc')
+                ->limit(10)
+                ->get();
+        });
 
         return view('homepage', ['products' => $products]);
     }
