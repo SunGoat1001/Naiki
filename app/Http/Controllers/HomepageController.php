96924339e13f43f49e492d63f7312664f81ba267
SunGoat1001 <ngoctien10012004@gmail.com>
2025-01-07 22:57:45 +0700

    Apply RateLimit, Batching

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -3,19 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Services\BatchingService;
 use Illuminate\Support\Facades\Cache;
 
 class HomepageController extends Controller
 {
+    protected $batchingService;
+
+    public function __construct(BatchingService $batchingService)
+    {
+        $this->batchingService = $batchingService;
+    }
+
     public function index()
     {
         $products = Cache::remember('homepage_data', 600, function () {
-            return Product::select('id', 'name', 'price', 'main_image_url')
+            $products = Product::select('id', 'name', 'price', 'main_image_url')
                 ->orderBy('imported_date', 'desc')
                 ->limit(8)
                 ->get();
+
+            return $this->batchingService->batchProducts($products, 2);
         });
 
-        return view('homepage', ['products' => $products]);
+        return view('homepage', ['products' => $products->flatten()]);
     }
 }
