9cb0f4f85f45b9c5354465374178120f1fd7d0fa
SunGoat1001 <ngoctien10012004@gmail.com>
2025-01-07 22:55:10 +0700

    Simplify controllers by removing unused model retrievals for views

--- app/Http/Controllers/WomenController.php
+++ app/Http/Controllers/WomenController.php
@@ -2,19 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
-use App\Models\Product;
-
 class WomenController extends Controller
 {
     public function index()
     {
-        $categories = Category::all();
-        $products = Product::all();
-
-        return view('women', [
-            'categories' => $categories,
-            'products' => $products,
-        ]);
+        return view('women');
     }
 }
